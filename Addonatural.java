da6698f9fae5661bbaa76d04cdd2855294aea279
Uddu <jagtapuddesh21@gmail.com>
2024-12-19 16:36:54 +0530

    MyfirstCommit

---
+++ Addonatural.java
@@ -0,0 +1,24 @@
+package practice;
+
+import java.util.Scanner;
+
+public class Addonatural {
+
+	
+
+	    public static void main(String[] args) {
+	    	Scanner sc = new Scanner(System.in);
+	        System.out.print("Enter a number: ");
+	        int num = sc.nextInt();
+	        
+	        int sum = 0;
+	        for (int i = 1; i <= num; i++) {
+	            sum += i;
+	        }
+	        
+	        System.out.println( sum);
+	        sc.close();
+	    }
+	}
+
+
